3182f1cf075c403d1e02ec43d617d27de1732fb9
Jesús Rivas Jiménez <jesus.rivas.jmnez@gmail.com>
2019-03-11 21:27:04 +0100

    @jesusrivasjmnez - Change version

--- scripts/merge_branch.sh
+++ scripts/merge_branch.sh
@@ -8,7 +8,7 @@ echo "Target branch: $TRAVIS_BRANCH"
 if [ "$TRAVIS_PULL_REQUEST" != "$NOT_PR" ]; then
   echo "Merging PR into $TRAVIS_BRANCH"
 
-  git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git
+  git remote set-url origin https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
 
   git fetch origin
   git checkout $TRAVIS_PULL_REQUEST_BRANCH
